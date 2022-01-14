c896c29ef6fd31151d093200c5b7d1d97c832edb
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-01-13 22:01:29 -0800

    Average.py

--- average.py
+++ average.py
@@ -1,1 +1,13 @@
-print("hello, world")
+# Author: Kimberly Rojas
+# GitHub username: kimberlyroj
+# Date: 1/13/2021
+# Description: A program that asks the user for five numbers and then prints out the average of those numbers.
+print("Please enter five numbers")
+first_num = float(input())
+second_num = float(input())
+third_num = float(input())
+fourth_num = float(input())
+fifth_num = float(input())
+result = first_num + second_num + third_num + fourth_num + fifth_num
+conversion_ratio = result/5
+print("The result is", conversion_ratio)
